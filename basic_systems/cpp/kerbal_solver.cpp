5b7f37f53dc446b42925ff16e5d10a4688111921
gautham-glitch <kaarthikgatham@gmail.com>
2026-06-28 08:04:00 +0530

    finally implemented the ut func

--- basic_systems/cpp/kerbal_solver.cpp
+++ basic_systems/cpp/kerbal_solver.cpp
@@ -1,2 +1,43 @@
-/* implementations will be given by this week, please wait until then for more code
-luckily, the planets.json file is not touched and shall be used for planetary constants*/
+#include <stdio.h>
+#include <eigen3/Eigen/Dense>
+#include <cmath>
+#include <algorithm>
+#include "nlohmann/json.hpp"
+#include <chrono>
+
+using namespace std;
+
+namespace constants {
+    const double pi = std::acos(-1);
+    const double ker_year_day = 426;
+    const double ker_day_hour = 6;
+    const double ker_hour_min = 60;
+    const double ker_min_sec = 60;
+    const double ker_year_sec = 9201600.0;
+    const double ker_day_sec = 21600.0;
+};
+
+double get_ut_secs(int year, int day, int hour, int minutes, int seconds, bool ker_time = false) {
+    if (ker_time) {
+        double elapsed_years = year - 1;
+        double elapsed_day = day - 1;
+        double total_secs = (elapsed_years * constants::ker_year_sec) + (elapsed_day * constants::ker_day_sec) + 
+        (hour * constants::ker_hour_min * constants::ker_min_sec) + (minutes * constants::ker_min_sec) + seconds;
+
+        return total_secs;
+    } else {
+        std::chrono::year_month_day base_ymd{
+            std::chrono::year{year},
+            std::chrono::month{1},
+            std::chrono::day{1}
+        };
+        
+        auto time_period = std::chrono::sys_days{base_ymd} + 
+                           std::chrono::days{day - 1} + 
+                           std::chrono::hours{hour} + 
+                           std::chrono::minutes{minutes} + 
+                           std::chrono::seconds{seconds};
+                           
+        return static_cast<double>(std::chrono::duration_cast<std::chrono::seconds>(time_period.time_since_epoch()).count());
+    }
+}
--- basic_systems/python/kerbal_solver.py
+++ basic_systems/python/kerbal_solver.py
@@ -1,2 +1,52 @@
-# plase wait untill next week, as the math has to be sorted out and the task corrected, please wait for the following week
-#luckily, the planets.json file is not touched and shall be used for planetary constants
+import numpy as np
+import datetime as dtime
+import numba as nb
+from math import pi as pi
+
+class Constants:
+    KER_YEAR_DAY = 426
+    KER_DAY_HOUR = 6
+    KER_HOUR_MIN = 60
+    KER_MIN_SEC = 60
+    KER_YEAR_SEC = 9201600.0
+    KER_DAY_SEC = 21600.0
+
+def get_ut_secs(year: int, day: int, hour: int, minute: int, seconds: int, ker_time = False):
+    """Calculate elapsed seconds since the epoch tracker.
+
+    Uses the KSP savefile start for Kerbal time, or the J2000 epoch 
+    for real-world time.
+
+    Args:
+        year: Year of the target timestamp.
+        day: Day of the year (1-indexed).
+        hour: Hour of the day (0-23).
+        minute: Minute of the hour (0-59).
+        seconds: Seconds of the minute (0-59).
+        ker_time: True to use Kerbin calendar rules, False for Earth.
+
+    Returns:
+        float: Total elapsed seconds.
+    """
+    
+    if ker_time:
+        elapsed_years = year - 1
+        elapsed_days = day - 1
+        
+        time_conversions = (
+            (elapsed_years, Constants.KER_YEAR_SEC),
+            (elapsed_days, Constants.KER_DAY_SEC),
+            (hour, Constants.KER_HOUR_MIN * Constants.KER_MIN_SEC),
+            (minute, Constants.KER_MIN_SEC),
+            (seconds, 1)
+        )
+
+        return float(sum(val * const for val, const in time_conversions))
+    
+    else:
+        j2000_epoch = dtime.datetime(2000, 1, 1, 12, 0, 0, tzinfo = dtime.timezone.utc)
+        base_date = dtime.datetime(year, 1, 1, tzinfo = dtime.timezone.utc)
+
+        target_date = base_date + dtime.timedelta(day - 1, hours = hour, minutes = minute, seconds = seconds)
+
+        return (target_date - j2000_epoch).total_seconds()
